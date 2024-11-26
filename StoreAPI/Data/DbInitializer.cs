a822da3bf9b9ec1d9cdcd2652741ae6e962a1822
Gingerbreadmanxxx <ginger.ranoso@motortrade.com.ph>
2024-11-26 19:19:44 +0800

    Added React application Section 3-21

--- StoreAPI/Data/DbInitializer.cs
+++ StoreAPI/Data/DbInitializer.cs
@@ -216,6 +216,8 @@ namespace StoreAPI.Data
           };
 
           context.Products.AddRange(products);
+
+          context.SaveChanges();
         }
     }
 }
---
+++ StoreClient/src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div>
+       <h1>TindahanStore</h1>
+    </div>
+  )
+}
+
+export default App
